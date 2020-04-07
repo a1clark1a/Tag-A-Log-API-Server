9c33e8dd83133f5e089d130bdd10e513fd0e407f
Anthony Clark Perfecto <a1clark1a@gmail.com>
2020-04-07 15:46:38 -0700

    app.js refactor

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
+
 const { NODE_ENV } = require("./config");
+const erroHandler = require("./middleware/error-handler");
 
 const app = express();
 
@@ -17,15 +19,6 @@ app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
-app.use(function erroHandler(error, req, res, next) {
-  let response;
-  if (NODE_ENV === "production") {
-    response = { error: { message: "server error" } };
-  } else {
-    console.error(error);
-    response = { message: error.message, error };
-  }
-  res.status(500).json(response);
-});
+app.use(erroHandler);
 
 module.exports = app;
